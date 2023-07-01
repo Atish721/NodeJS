4504ac8130ba4345bfb4d3d71e4ea17a18fe5bf9
Atish Chandole <auto91akc@gmail.com>
2023-07-02 00:42:04 +0530

    CRUD with API

--- API/src/Models/Items.js
+++ API/src/Models/Items.js
@@ -3,21 +3,47 @@ const { getDb } = require('./Database')
 
 const collectionName = 'Items'
 
-class Items{
-    constructor(data)
-    {
-        this.Name=data.Name,
-        this.Category=data.Category,
-        this.Price=data.Price,
-        this.Quantity=data.Quantity
+class Items {
+    constructor(data) {
+        this.Name = data.Name,
+            this.Category = data.Category,
+            this.Price = data.Price,
+            this.Quantity = data.Quantity
     }
 
-    static async getAll()
-    {
+    static async getAll() {
         const db = getDb()
         const collection = db.collection(collectionName);
         const items = await collection.find({}).toArray()
         return items
+    }
+
+    static async getByWhereClause(whereClause) {
+        const db = getDb()
+        const collection = db.collection(collectionName)
+        const item = await collection.findOne(whereClause)
+        return item
+    }
+
+    async save() {
+        const db = getDb()
+        const collection = db.collection(collectionName);
+        const result = await collection.insertOne(this)
+        return result.insertedId
+    }
+
+    async update(id) {
+        const db = getDb();
+        const collection = db.collection(collectionName);
+        const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: { Name: this.Name } });
+        return result.modifiedCount;
+    }
+
+    static async delete(id) {
+        const db = getDb();
+        const collection = db.collection(collectionName);
+        const result = await collection.deleteOne({ _id: new ObjectId(id) });
+        return result.deletedCount;
     }
 }
 
--- API/src/Routes/Item.js
+++ API/src/Routes/Item.js
@@ -2,22 +2,134 @@ const express = require('express')
 const router = express.Router()
 const Items = require('../Models/Items')
 
+//Validation
+const itemValidation = (request, response, next) => {
+    const { Name, Category, Price, Quantity } = request.body;
+
+    if (!Name || typeof Name !== 'string') {
+        return response.status(400).json({ error: 'Name is required and must be a string' })
+    }
+
+    if (!Category || typeof Category !== 'string') {
+        return response.status(400).json({ error: 'Category is required and must be a string' })
+    }
+
+    if (typeof Price !== 'number') {
+        if (parseInt(Price) < 0) {
+            return response.status(400).json({ error: 'Price must be greater than 0' })
+        }
+
+        return response.status(400).json({ error: 'Price is required and must be a number' })
+    }
+
+    if (typeof Quantity !== 'number') {
+        if (parseInt(Quantity) < 0) {
+            return response.status(400).json({ error: 'Quantity must be greater than 0' })
+        }
+
+        return response.status(400).json({ error: 'Quantity is required and must be a number' })
+    }
+    next()
+}
+
 //Get all items
 router.get('/get_all', async (request, response) => {
     try {
         const items = await Items.getAll()
-        if(items.lenght>0)
-        {
+
+        if (items.length > 0) {
             response.status(200).json(items)
-        }            
-        else
-        {
-            response.status(404).json({Message:'Items not found.'})
+        }
+        else {
+            response.status(404).json({ Message: 'Items not found.' })
         }
     }
     catch (error) {
         console.error('Error retrieving items:', error);
         response.status(500).json({ error: 'Error retrieving items' });
+    }
+})
+
+//Get item by where clause
+router.post('/get', async (request, response) => {
+    try {
+        const whereClause = request.body;
+        const item = await Items.getByWhereClause(whereClause);
+        if (item) {
+            response.status(200).json(item);
+        } else {
+            response.status(404).json({ error: 'Item not found' });
+        }
+    } catch (error) {
+        console.error('Error retrieving item:', error);
+        res.status(500).json({ error: 'Error retrieving item' });
+    }
+});
+
+
+//Insert item 
+router.post('/insert', itemValidation, async (request, response) => {
+    try {
+        const newItem = new Items(request.body)
+        const existingItem = await Items.getByWhereClause({ Name: request.body.Name })
+
+        if (existingItem) {
+            response.status(200).json({ Message: 'Item with the same name already exists', Data: existingItem })
+        }
+        else {
+
+            const insertId = await newItem.save()
+            response.status(200).json({ Message: 'Item saved', Data: { _id: insertId } })
+
+        }
+
+    } catch (error) {
+        console.error('Error on save item:', error);
+        response.status(500).json({ error: 'Error save items' });
+    }
+})
+
+
+//Update item 
+router.put('/update', async (request, response) => {
+    try {
+        const updatedItem = new Items(request.body)
+        const existingItem = await Items.getByWhereClause({ Name: request.body.Name })
+        if (existingItem && existingItem._id.toString() !== request.body.Id) {
+            response.status(200).json({ Message: 'Item with the same name already exists', Data: existingItem })
+        }
+        else {
+
+            const modifiedCount = await updatedItem.update(request.body.Id)
+            if (modifiedCount > 0) {
+                response.status(200).json({ Message: 'Item updated' })
+            } else {
+                response.status(404).json({ error: 'Item not found' });
+            }
+
+        }
+
+    } catch (error) {
+        console.error('Error on update item:', error);
+        response.status(500).json({ error: 'Error update items' });
+    }
+})
+
+
+// Delete an item
+router.delete('/delete', async (request, response) => {
+    try {
+        const itemId = request.body.Id
+        const deletedCount = await Items.delete(itemId)
+        if (deletedCount > 0) {
+            response.status(200).json({ Message: 'Item deleted successfully' })
+        } else {
+            response.status(404).json({ error: 'Item not found' });
+        }
+
+    } catch (error) {
+        console.error('Error on deleting item:', error);
+        response.status(500).json({ error: 'Error deleting items' });
     }
 })
 
